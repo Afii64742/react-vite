9978e9fa506ea31d2ffd3ac8dc7ae4f31ead0eca
Aftab64742 <afii64742@gmail.com>
2025-04-16 11:23:06 +0500

    more bugs removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
+
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-     <h1>Hello Welcome to React app, Deployed on AWS AMLIFY with Auto-Deployment... :-)</h1>
+     <h1>Hello Welcome to React app, Deployed on AWS AMLIFY with Auto-Deployment...</h1>
     </>
   )
 }
